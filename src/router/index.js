9992e1939a179f3631a051edd70f0ba12b25a33a
Afrizal <afrizalbagas.03@gmail.com>
2022-04-06 23:04:38 +0700

    add homescreen router

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,19 @@
 import {createStackNavigator} from '@react-navigation/stack';
+import {createDrawerNavigator} from '@react-navigation/drawer';
 import React from 'react';
-import {Splash, Login, Register, WelcomeAuth} from '../pages';
+import {Splash, Login, Register, WelcomeAuth, Home} from '../pages';
 
 const Stack = createStackNavigator();
+const Drawer = createDrawerNavigator();
+
+const MainApp = () => {
+  return (
+    <Drawer.Navigator initialRouteName="Home">
+      <Drawer.Screen name="Home" component={Home} />
+    </Drawer.Navigator>
+  );
+};
+
 const Router = () => {
   return (
     <Stack.Navigator initialRouteName="Splash">
@@ -24,6 +35,11 @@ const Router = () => {
       <Stack.Screen
         name="WelcomeAuth"
         component={WelcomeAuth}
+        options={{headerShown: false}}
+      />
+      <Stack.Screen
+        name="MainApp"
+        component={MainApp}
         options={{headerShown: false}}
       />
     </Stack.Navigator>
